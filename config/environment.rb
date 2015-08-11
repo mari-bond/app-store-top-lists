b16e048a25f5c1bce1050141d2b2138ac9a4bc0b
Bondarenko Marina <marina.gurbenko@gmail.com>
2015-08-11 17:56:24 +0300

    Redis configuration

--- config/environment.rb
+++ config/environment.rb
@@ -8,4 +8,10 @@ Bundler.require(Sinatra::Base.environment)  # load all the environment specific 
 require "active_support/deprecation"
 require "active_support/all"
 
-$redis = Redis.new
+if ENV["REDISTOGO_URL"]
+  redis_uri = URI.parse(ENV["REDISTOGO_URL"])
+  options = {:host => redis_uri.host, :port => redis_uri.port, :password => redis_uri.password}
+else
+  options = {}
+end
+$redis = Redis.new(options)
